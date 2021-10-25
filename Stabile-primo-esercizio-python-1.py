f6a6573c7b9ca6fe4292b8e5331aa9c21fc2380f
stabilesara <91075641+stabilesara@users.noreply.github.com>
2021-10-25 22:27:04 +0200

    Add files via upload

---
+++ Stabile-primo-esercizio-python-1.py
@@ -0,0 +1,58 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+x=8
+y=3
+
+print(x + y)
+11
+
+x=57
+y=19
+
+print(x - y)
+38
+
+
+x=5
+y=3
+
+print(5 * 3)
+15
+
+
+x=28
+y=4
+
+print(x / y)
+7.0
+
+
+x=20
+y=7
+
+print(x % y)
+6
+
+
+x=3
+y=4
+
+print(x ** y)
+81
+
+
+x=43
+y=10
+
+print(x // y)
+4
+4
+4
+
+
+4
+4
+
+
+
