f3b5f9079aaf58cdd6c271971b0fdf9e42cae820
stabilesara <91075641+stabilesara@users.noreply.github.com>
2021-11-16 00:43:44 +0100

    Add files via upload

---
+++ stabile_costruttodiselezione.py
@@ -0,0 +1,16 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+A=7
+B=5
+C=3
+
+if A > B:
+    print('A è il più grande')
+elif A < C:
+    print ('C è il più grande')
+else:
+    print >(A)
+
+    
+A è il più grande
