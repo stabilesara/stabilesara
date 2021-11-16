b8dac7940c5213f94f139c7ec52c7700d3e961e0
stabilesara <91075641+stabilesara@users.noreply.github.com>
2021-11-16 14:25:33 +0100

    Add files via upload

---
+++ esercizio-02.py
@@ -0,0 +1,14 @@
+A= input("inserisci un numero: ")
+B= input("inserisci un numero: ")
+C= input("inserisci un numero: ")
+
+if A>B and A>C:
+    print("il più grande è",A)
+
+elif B>A and B>C:
+    print("il più grande è",B)
+
+else:
+    print("il più grande è",C)
+    
+    
